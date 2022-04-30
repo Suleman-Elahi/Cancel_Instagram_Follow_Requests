d03b5c5fa3b9d14cc72edafc78b14c1128c3a655
Suleman <slmn95@rediffmail.com>
2022-04-30 15:17:03 +0530

    Update Selenium Command and "View More" button issue.

--- get_follow_req.py
+++ get_follow_req.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import time
 import loginInfo
 import os
 
-options = Options()
+options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920x1080")
 options.add_argument("--incognito")
 chromedriver_path = './chromedriver'
 
 f = open("follow_req.txt","w")
 
-browser = webdriver.Chrome(executable_path=chromedriver_path, options=options) #Star Browser
+s=Service('./chromedriver.exe')
+browser = webdriver.Chrome(service=s, options=options)
 
 browser.get("https://www.instagram.com/")
 
@@ -20,13 +22,13 @@ time.sleep(3) #Waiting 3 seconds after we open the page.
 
 #IG Login -->
 
-username=browser.find_element_by_name ("username")
+username=browser.find_element(By.NAME, "username")
 username.send_keys (loginInfo.username)
 
-password =browser.find_element_by_name ("password")
+password =browser.find_element(By.NAME, "password")
 password.send_keys(loginInfo.password)
 
-login_button = browser.find_element_by_xpath ("//button[@type='submit']")
+login_button = browser.find_element(By.XPATH, "//button[@type='submit']")
 login_button.click()
 time.sleep(6)
 
@@ -34,13 +36,13 @@ browser.get("https://www.instagram.com/accounts/access_tool/current_follow_reque
 
 while True:
     try:
-        vm_button = browser.find_element_by_xpath ("//button[@type='button']")
+        vm_button = browser.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/article/main/button')
         vm_button.click()
         time.sleep(2)
     except NoSuchElementException:
         break
 
-cfreq_source = browser.find_elements_by_xpath("//div[@class='-utLf']") 
+cfreq_source = browser.find_elements(By.XPATH, "//div[@class='-utLf']") 
 
 count=0
 
